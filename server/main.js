6b2e6d06224eceb6a0543017fdffbcf3b308dae2
root <root@ip-172-31-23-44.ec2.internal>
2018-12-12 13:55:43 +0000

    import mysql

--- server/main.js
+++ server/main.js
@@ -1,5 +1,24 @@
 var express = require('express');
+
+var mysql = require('mysql');
+
 var app = express();
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'qwe123', 
+  database : 'mop'
+});
+ 
+connection.connect(function(err){
+   if (err) throw err;
+   console.log("Mysql Connect")
+});
+
+
+
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 })
