bcf629a7776a4b5dcaac2edebbf8d1114e7acd1c
PKH\arlec <gh9411@gmail.com>
2018-12-15 23:55:58 +0900

    move log to comment

--- client/app/src/main/java/com/example/arlec/myapplication/user_main.java
+++ client/app/src/main/java/com/example/arlec/myapplication/user_main.java
@@ -2,7 +2,6 @@ package com.example.arlec.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import java.util.concurrent.ExecutionException;
 
@@ -17,7 +16,7 @@ public class user_main extends AppCompatActivity {
         try {
             String proudct_db = downloadTask.execute("http://ec2-54-91-46-126.compute-1.amazonaws.com:3000/product").get();
 
-            Log.d("프로덕트디비",proudct_db);
+            //Log.d("프로덕트디비",proudct_db);
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
