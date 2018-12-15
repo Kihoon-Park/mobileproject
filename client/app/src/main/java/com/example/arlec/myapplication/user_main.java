d8e57d0796f085e6c38bda47dcdd5b4e56435658
PKH\arlec <gh9411@gmail.com>
2018-12-16 00:40:50 +0900

    define item.xml for product list

--- client/app/src/main/java/com/example/arlec/myapplication/user_main.java
+++ client/app/src/main/java/com/example/arlec/myapplication/user_main.java
@@ -3,6 +3,9 @@ package com.example.arlec.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.concurrent.ExecutionException;
 
 public class user_main extends AppCompatActivity {
@@ -15,7 +18,10 @@ public class user_main extends AppCompatActivity {
         GetData.DownloadTask downloadTask = new GetData.DownloadTask();
         try {
             String proudct_db = downloadTask.execute("http://ec2-54-91-46-126.compute-1.amazonaws.com:3000/product").get();
-
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+            Product[] products = gson.fromJson(proudct_db, Product[].class);
             //Log.d("프로덕트디비",proudct_db);
         } catch (ExecutionException e) {
             e.printStackTrace();
