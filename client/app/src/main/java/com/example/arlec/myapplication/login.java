d243aca8d99df5d112e591050dd5155351676152
PKH\arlec <gh9411@gmail.com>
2018-12-15 23:49:39 +0900

    relining

--- client/app/src/main/java/com/example/arlec/myapplication/login.java
+++ client/app/src/main/java/com/example/arlec/myapplication/login.java
@@ -36,27 +36,29 @@ public class login extends AppCompatActivity {
                     Gson gson = new GsonBuilder()
                                 .setLenient()
                                 .create();
-                        User[] users = gson.fromJson(user_db, User[].class);
+                    User[] users = gson.fromJson(user_db, User[].class);
 
-                        EditText u_id_text = (EditText) findViewById(R.id.u_id_text);
-                        EditText u_pw_text = (EditText)findViewById(R.id.u_pw_text);
-                        String id = u_id_text.getText().toString();
-                        String pw = u_pw_text.getText().toString();
-                        int check = 0;
-                        for(int x = 0;x< users.length;x++){
-                            if( users[x].getName().equals(id) && users[x].getPassword().equals(pw)){
-                                check = 1;
-                                break;
-                            }
-                            else
-                                Toast.makeText(getBaseContext(),
-                                        "ID와 비밀번호를 다시 확인해 주십시오.", Toast.LENGTH_SHORT)
-                                        .show();
+                    EditText u_id_text = (EditText) findViewById(R.id.u_id_text);
+                    EditText u_pw_text = (EditText)findViewById(R.id.u_pw_text);
+                    String id = u_id_text.getText().toString();
+                    String pw = u_pw_text.getText().toString();
+                    int check = 0;
+
+                    for(int x = 0;x< users.length;x++) {
+                        if (users[x].getName().equals(id) && users[x].getPassword().equals(pw)) {
+                            check = 1;
+                            break;
                         }
+                    }
                     if(check == 1) {
                         Intent intent = new Intent(
                                 getApplicationContext(), user_main.class); // define next class
                         startActivity(intent); // change next view
+                    }
+                    else {
+                        Toast.makeText(getBaseContext(),
+                                "ID와 비밀번호를 다시 확인해 주십시오.", Toast.LENGTH_SHORT)
+                                .show();
                     }
                 } catch (ExecutionException e) {
                     e.printStackTrace();
@@ -66,31 +68,5 @@ public class login extends AppCompatActivity {
             }
         });
     }
-    /*
 
-    protected void onPostExecute(String result) {
-        Log.d("where?" , result);
-        TextView tView = (TextView) findViewById(R.id.textView);
-        tView.setText(result);
-        Toast.makeText(getBaseContext(),
-                "Web page downloaded successfully", Toast.LENGTH_SHORT)
-                .show();
-
-        Log.i("RESPONSE", result);
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-        //Student [] studes = gson.fromJson("[{\"name\": \"kim\", \"email\": \"kim@gmail.com\", \"phone\": \"010-1234-0001\", \"age\": 10}]", Student[].class);
-        User[] users = gson.fromJson(result, User[].class);
-        for (User s : users) {
-            Log.i("U_ID", s.getName());
-            Log.i("U_PASSWORD", s.getPassword());
-
-            System.out.println(s.getName());
-
-        }
-
-
-    }
-*/
 }
