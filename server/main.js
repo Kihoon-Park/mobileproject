178a038fe5d9fd1c8f30ff6f57bb255523a0f0f0
PKH\arlec <gh9411@gmail.com>
2018-12-16 00:02:49 +0900

    Redefine main.js (error reset)

--- server/main.js
+++ server/main.js
@@ -1,28 +1,52 @@
 var express = require('express');
-
 var mysql = require('mysql');
-
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'qwe123', 
+  password : 'qwe123',
   database : 'mop'
 });
- 
+
 connection.connect(function(err){
    if (err) throw err;
    console.log("Mysql Connect")
 });
-
-
 
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 })
 
 app.get('/',function(req,res){
-	res.send("Hello Mop")
+        res.send("Hello Mop")
 });
+
+
+//user REST API
+app.get("/user", function(req, res){
+    connection.query('SELECT * from user', function(err, rows, fields){
+        if (!err){
+            console.log('The solution is: ', rows);
+            res.json(rows);
+        }
+        else
+            console.log('Error while performing Query.', err);
+    });
+})
+
+//product REST API
+app.get("/product", function(req, res){
+    connection.query('SELECT * from product', function(err, rows, fields){
+        if (!err){
+            console.log('The solution is: ', rows);
+            res.json(rows);
+        }
+        else
+            console.log('Error while performing Query.', err);
+    });
+})
